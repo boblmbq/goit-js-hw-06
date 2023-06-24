090ab2bd6e86a3ae09d691eb1d77cd72ab5f65e8
boblmbq <sichkardanylo@gmail.com>
2023-06-24 20:09:35 +0200

    task 2 end

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,24 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const allLi = [];
+
+ingredients.forEach((el) => {
+  const createLi = document.createElement("li");
+  createLi.textContent = el;
+  createLi.classList.add(".item")
+  allLi.push(createLi)
+});
+
+console.log(allLi)
+
+const ul = document.querySelector("#ingredients")
+ul.append(...allLi)
+
+console.log(ul)
