d54816e1ad820ac5fe89be61e6f65a12179281ee
boblmbq <sichkardanylo@gmail.com>
2023-06-24 19:22:45 +0200

    first task done

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,27 +1,13 @@
-// Порахує і виведе в консоль кількість категорій
-// в ul#categories, тобто елементів li.item.
-
-// Для кожного элемента li.item
-// у списку ul#categories, знайде і виведе
-// в консоль текст заголовку елемента(тегу < h2 >)
-// і кількість елементів в категорії(усіх < li >, вкладених в нього).
-
-// Number of categories: 3
-
-// Category: Animals
-// Elements: 4
-
-// Category: Products
-// Elements: 3
-
-// Category: Technologies
-// Elements: 5
-
 const ul = document.querySelector("#categories");
-console.log(ul);
+const children = ul.querySelectorAll(".item");
 
-const childFromUl = ul.querySelectorAll(".item");
-console.log(childFromUl);
+console.log(`Number of categories: ${children.length}`);
+console.log("\n");
 
-const numOfEL = childFromUl.length;
-console.log(`Number of categories: ${numOfEL}`);
+const func = children.forEach((el) => {
+  const h2Text = el.querySelector("h2").textContent; //Animals Products Technologies
+  const categories = el.querySelectorAll("li").length;
+  console.log(`Category: ${h2Text}`);
+  console.log(`Elements: ${categories}`);
+  console.log("\n");
+});
