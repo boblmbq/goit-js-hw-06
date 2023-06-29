6b78b2047e89a47cb0e1860fc4d3d004672c3a29
boblmbq <sichkardanylo@gmail.com>
2023-06-29 12:24:57 +0200

    Revert "all tasks are done"
    This reverts commit 18ac13f24218d55ada89f1f0d96447c3797f29a3.

--- js/task-09.js
+++ js/task-09.js
@@ -1,20 +1,5 @@
-// Напиши скрипт, який змінює кольори фону
-// елемента < body > через інлайн - стиль по
-// кліку на button.change - color і виводить
-// значення кольору в span.color.
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
-
-const btn = document.querySelector(".change-color");
-const span = document.querySelector(".color");
-
-function onClickBtn() {
-  document.body.style.backgroundColor = getRandomHexColor();
-  span.textContent = getRandomHexColor();
-}
-
-btn.addEventListener("click", onClickBtn)
--- js/task-10.js
+++ js/task-10.js
@@ -1,57 +1,5 @@
-// Розміри найпершого <div> - 30px на 30px.
-// Кожен елемент після першого повинен бути ширшим
-// і вищим від попереднього на 10px.
-
-// Всі елементи повинні мати випадковий колір фону
-// у форматі HEX.Використовуй готову функцію
-// getRandomHexColor для отримання кольору.
-
-document.body.style.backgroundColor = "black";
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
-
-const refs = {
-  input: document.querySelector("input[type=number]"),
-  btnDestroy: document.querySelector("button[data-destroy]"),
-  btnCreate: document.querySelector("button[data-create]"),
-  div: document.getElementById("boxes"),
-};
-const { input, btnDestroy, btnCreate, div } = refs;
-
-const inputMin = Number(input.min);
-const inputMax = Number(input.max);
-
-btnDestroy.addEventListener("click", () => {
-  div.innerHTML = "";
-});
-
-function createBoxes(amount) {
-  if (amount < inputMin || amount > inputMax) {
-    alert(
-      `${amount} is not in limit. The limit is from ${inputMin} to ${inputMax}`
-    );
-    input.value = "";
-    return;
-  }
-
-  const boxes = [];
-  for (let i = 0; i < amount; i++) {
-    const divEl = document.createElement("div");
-    divEl.style.backgroundColor = getRandomHexColor();
-    divEl.style.width = `${30 + i * 10}px`;
-    divEl.style.height = `${30 + i * 10}px`;
-    boxes.push(divEl);
-  }
-  div.append(...boxes);
-}
-
-const onCreateClick = () => {
-  const lol = Number(input.value);
-  createBoxes(lol);
-};
-
-btnCreate.addEventListener("click", onCreateClick);
