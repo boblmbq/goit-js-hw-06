1d87ff98341d70c5237aea4239233f23ac3d24d0
boblmbq <sichkardanylo@gmail.com>
2023-06-30 16:56:06 +0200

    .

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,15 @@
-const ul = document.querySelector("#categories");
-const children = ul.querySelectorAll(".item");
+// const ul = document.querySelector("#categories");
+// const children = ul.querySelectorAll(".item");
 
-console.log(`Number of categories: ${children.length}`);
-console.log("\n");
+// console.log(`Number of categories: ${children.length}`);
+// console.log("\n");
 
-const func = children.forEach((el) => {
-  const h2Text = el.querySelector("h2").textContent; //Animals Products Technologies
-  const categories = el.querySelectorAll("li").length;
-  console.log(`Category: ${h2Text}`);
-  console.log(`Elements: ${categories}`);
-  console.log("\n");
-});
+// const func = children.forEach((el) => {
+//   const h2Text = el.querySelector("h2").textContent; //Animals Products Technologies
+//   const categories = el.querySelectorAll("li").length;
+//   console.log(`Category: ${h2Text}`);
+//   console.log(`Elements: ${categories}`);
+//   console.log("\n");
+// });
+
+
